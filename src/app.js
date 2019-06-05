be6417dd8c5ae259bf32bbccde590e47d6fd0d3c
rajan4998 <rajan4998@gmail.com>
2019-06-05 15:30:53 +0530

    handlebbars templating integrated

--- src/app.js
+++ src/app.js
@@ -8,7 +8,16 @@ const app = express()
 
 const publicDirectoryPath = path.join(__dirname,'../public')
 
+app.set('view engine','hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title : 'Help view',
+        message : 'This is help view',
+        name: 'Rajan Joshi'
+    })
+})
 
 app.get('/weather',(req, res)=>{
     let coordinates = {
@@ -16,8 +25,22 @@ app.get('/weather',(req, res)=>{
         longitude: 77.704608,
         location : 'Meerut'
     }
-
+    
     res.send(coordinates)
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title : 'About',
+        name: 'Rajan Joshi'
+    })
+})
+
+app.get('/',(req,res)=>{
+    res.render('index',{
+        title : 'Weather App',
+        name : 'Rajan Joshi'
+    })
 })
 
 app.get('/*',(req,res)=>{

01ae83314cac060ee90f6466463aa9808a7a65fa
rajan4998 <rajan4998@gmail.com>
2019-06-07 18:17:05 +0530

    extra console and params removed

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,7 @@
 console.log("This is static js file")
 
 const url = "http://localhost:3000/products?address=meerut"
-fetch(url, {
-    mode: 'cors',
-    headers: {
-        'Access-Control-Allow-Origin': '*'
-    }
-    }).then((response)=>{
+fetch(url).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             console.log(data.error)

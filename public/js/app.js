18f68023db6b72ba73efc5e0d5a30a158b5807ab
rajan4998 <rajan4998@gmail.com>
2019-06-07 18:30:26 +0530

    geocode and weatherforecast api triggered on form submit

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,19 @@
-console.log("This is static js file")
+const weatherForm = document.querySelector('form')
+weatherForm.addEventListener('submit',(e)=>{
 
-const url = "http://localhost:3000/products?address=meerut"
-fetch(url).then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error)
-        }else{
-            console.log(data.forecast)
-        }
+    const search = document.querySelector('input')
+    const location = search.value
+    const url = "http://localhost:3000/products?address=" + location
+
+    e.preventDefault()
+
+    fetch(url).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                console.log(data.error)
+            }else{
+                console.log(data.forecast)
+            }
+        })
     })
 })
